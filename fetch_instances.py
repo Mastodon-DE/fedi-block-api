9f51b67a6a1d4c3e87895a604cb6d8b48a6eb1c3
Nick Rudak <rudak@denic.de>
2023-09-26 02:39:27 +0200

    Refactored fetch_instances.py

--- fetch_instances.py
+++ fetch_instances.py
@@ -1,8 +1,6 @@
-#from reqto import get
 from hashlib import sha256
 from multiprocessing import Lock, cpu_count, set_start_method, Process #Pool,
 from httpx import AsyncClient
-import json
 import aiohttp
 import asyncio
 import sqlite3
@@ -17,7 +15,6 @@ async def get_peers(domain: str) -> str:
         async with aiohttp.ClientSession() as session:
             res  = await session.get(f"https://{domain}/api/v1/instance/peers") #, headers=headers, timeout=5, allow_redirects=False)
             resj = await res.json()
-            #print(resj)
             return resj
     except Exception as e:
         print(e)
@@ -52,13 +49,7 @@ async def get_type(instdomain: str) -> str:
             if res.ok:
                 return "mastodon"
     except Exception as e:
-        #print(e)
         return None
-
-async def get_response_code(host:str) -> int:
-    async with aiohttp.ClientSession() as session:
-        res = await session.get(f"https://{host}/nodeinfo/2.1.json")#, headers=headers, timeout=5, allow_redirects=False)
-        print(f"got response code {res.status}")
 
 async def write_instance(instance: str, c) -> bool:
     try:
@@ -78,7 +69,6 @@ async def write_instance(instance: str, c) -> bool:
     return True
 
 async def main():
-    #Initial importing of Global configurations
     global config
     global headers
     global domain
@@ -86,7 +76,7 @@ async def main():
     with open("config.json") as f:
         config = json.loads(f.read())
     headers    = {"user-agent": config["useragent"]}
-    domain     = "mastodon.de" #sys.argv[1]
+    domain     = sys.argv[1]
     conn = sqlite3.connect("blocks.db")
     c = conn.cursor()
     peerlist =  await get_peers(domain)
@@ -102,80 +92,9 @@ async def main():
                 continue
 
             tg.create_task(write_instance(peer, c))
-            #tg.create_task(get_response_code(peer))
     conn.close()
-
-
 
 if __name__ == "__main__":
     asyncio.run(main())
 
 sys.exit()
-
-with open("config.json") as f:
-    config = json.loads(f.read())
-
-
-blacklist = [
-    "activitypub-troll.cf",
-    "gab.best",
-    "4chan.icu",
-    "social.shrimpcam.pw",
-    "mastotroll.netz.org",
-    "github.dev",
-    "ngrok.io"
-]
-
-headers = {
-    "user-agent": config["useragent"]
-}
-
-peerlist = get_peers(domain)
-
-conn = sqlite3.connect("blocks.db")
-
-c = conn.cursor()
-
-c.execute(
-    "select domain from instances where 1"
-)
-print("run2")
-#(cpu_count() - 1)
-print("run3")
-mutex = Lock()
-
-#This one will create a pool of processes
-#With the same number as cpus on the host
-#But minus 1 for the os
-#This should hopefully fix accidentally
-#creating forkbombs and crashing the kernel
-#(it was fun though)
-
-
-for instance in peerlist:
-    instance = instance.lower()
-
-    blacklisted = False
-    for ddomain in blacklist:
-        if ddomain in instance:
-            blacklisted = True
-
-    if blacklisted:
-        continue
-    print("run1")
-    #p = pool.apply(write_instance, args=[instance, c])
-    #print(p.get())
-
-    #p = Process(target=write_instance, args=[instance, c])
-    #p.start()
-    #Funny story about that
-    #Thats a fork bomb do not run that
-    #Except for lolz thats why I didnt delete it
-
-    #write_instance(instance, c)
-    #DEBUG PLEASE REMOVE AFTER USE
-    #print(instance) #REMOVE AFTER USE OR PERFOMANCE IS GONNA TANK
-    #DEBUG PLEASE REMOVE AFTER USE
-
-conn.close()
-print("done " + domain)
